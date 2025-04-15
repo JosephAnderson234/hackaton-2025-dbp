90a5383a6c36a205c22511cdab6ed5fae992e064
JosephAnderson234 <joseph.cose@utec.edu.pe>
2025-04-15 17:38:17 -0500

    feat: Añadido la funcionalidad de division

--- src/main.py
+++ src/main.py
@@ -1,2 +1,4 @@
-def calculate() -> float:
-    pass
+def calculate(entrada) -> float:
+    if '/' in entrada:
+        partes = entrada.split('/')
+        resultado = float(partes[0]) / float(partes[1])
