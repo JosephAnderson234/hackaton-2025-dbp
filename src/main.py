9c8429848eb47ea9b0bf46360f1f12c8f96f2c4e
KarolayTamayoH <karolaytamayo20@gmail.com>
2025-04-15 17:36:08 -0500

    feat(main.py): AÑADÍ LA FUNCIÓN RESTA

--- src/main.py
+++ src/main.py
@@ -1,2 +1,9 @@
 def calculate() -> float:
     pass
+
+if '-' in entrada:
+        partes = entrada.split('-')
+        if len(partes) == 2:
+                num1 = float(partes[0])
+                num2 = float(partes[1])
+                return num1 - num2
