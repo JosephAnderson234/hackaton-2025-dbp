f0d456fc51333b478b8b2c93949f5a0a3df14267
Daros <jose.huaman@utec.edu.pe>
2025-04-15 17:36:06 -0500

    feat(main.py): Añadi la operacion suma

--- src/main.py
+++ src/main.py
@@ -1,2 +1,7 @@
-def calculate() -> float:
-    pass
+def calculate(entrada):
+    if '+' in entrada:
+        partes = entrada.split('+')
+        if len(partes) == 2:
+            num1 = float(partes[0])
+            num2 = float(partes[1])
+            return num1 + num2
