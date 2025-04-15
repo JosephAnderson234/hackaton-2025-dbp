fab269344c2833bc2d3edb2c82e23720fb85f3cd
araooz <araozfali@gmail.com>
2025-04-15 17:36:02 -0500

    feat(main.py): añadi la operacion multiplicacion

--- src/main.py
+++ src/main.py
@@ -1,2 +1,8 @@
-def calculate() -> float:
-    pass
+def calculate(entrada) -> float:
+    entrada = entrada.strip()
+    if '*' in entrada:
+        partes = entrada.split('*')
+        if len(partes) == 2:
+            num1 = float(partes[0])
+            num2 = float(partes[1])
+            return num1 * num2
